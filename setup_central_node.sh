89fe8091a5c2e60dc12def6eb517874bf5ecc384
AskoRBNIKAs <laeryar2209@gmail.com>
2026-04-16 12:04:09 +0300

    added .env generation for collector

--- setup_central_node.sh
+++ setup_central_node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-echo "[*] Starting containers"
+echo "[*] Starting RabbitMQ and ManticoreSearch"
 
-docker compose -f docker-compose.central_node.yml up -d
+docker compose -f docker-compose.central_node.yml up manticore rabbitmq -d
 docker compose -f docker-compose.central_node.yml exec -T manticore mysql -h127.0.0.1 -P9306 < manticore/create_tables.sql
 sleep 10
 
@@ -15,5 +15,10 @@ echo "[*] Generated password for logger: ${RABBITMQ_PASSWORD}"
 docker compose -f docker-compose.central_node.yml exec rabbitmq rabbitmqctl add_vhost logs
 docker compose -f docker-compose.central_node.yml exec rabbitmq rabbitmqctl add_user logger "${RABBITMQ_PASSWORD}"
 docker compose -f docker-compose.central_node.yml exec rabbitmq rabbitmqctl set_permissions -p logs logger ".*" ".*" ".*"
+
+echo "RABBITMQ_USER=logger\n" >> .env.collector
+echo "RABBITMQ_PASSWORD=${RABBITMQ_PASSWORD}" >> .env.collector
+
+echo "[*] Starting Log Collector"
 
 echo "[+] Enjoy ;)" 
